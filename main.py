5d223ef69d982461b1f06a576bbdc10546946e16
Alex <aaugustine10@gmail.com>
2020-02-22 18:48:10 -0500

    Save off params as constant

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from flask import Flask, request, abort
 
 app = Flask(__name__)
-
+DATA_FROM_WEBHOOK = 'Waiting for data...'
 
 @app.route('/')
 def main():
-    return 'Hello, World!'
+    return DATA_FROM_WEBHOOK
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     if request.method == 'POST':
         res = request.json
         print(res)
+        DATA_FROM_WEBHOOK = res
         return res, 200
     else:
         abort(400)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
