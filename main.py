ffc8fc375a7972a3f8a5f9557891791f41d38f7a
Alex <aaugustine10@gmail.com>
2020-02-22 18:14:24 -0500

    return post parmas

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ def main():
 @app.route('/webhook', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        print(request.json)
-        return '', 200
+        res = request.json
+        print(res)
+        return res, 200
     else:
         abort(400)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
